87d2f56549672a43a04c8c0a9c45a87f30ae6151
SrijalKarmacharya <86567992+SrijalKarmacharya@users.noreply.github.com>
2021-08-18 13:24:58 +0545

    Create project1.groovy

---
+++ vars/project1.groovy
@@ -0,0 +1,21 @@
+def call(body) {
+  def pipelineParams= [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = pipelineParams
+  body()
+  
+ pipeline {
+  agent any 
+  
+  stages {
+   stage('check') {
+    steps {
+     script {
+      building.add(60)
+     }
+    }
+   }
+  }
+ }
+}
+  
